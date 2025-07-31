157185724e79503545d6863768b7536fe8ac5254
JAFT3 <fernandezdelatorrejesusantonio@gmail.com>
2025-07-31 12:57:21 -0600

    Correccion del error que no permitia almacenar datos en la segunda tabla por falta de campos y a su vez enlazamiento de las tablas de datos.

--- src/BankManagementSystem/ui/SignUpTwo.java
+++ src/BankManagementSystem/ui/SignUpTwo.java
@@ -201,8 +201,8 @@ public class SignUpTwo extends JFrame implements ActionListener{
                 JOptionPane.showMessageDialog(null, "Todos los campos son obligatorios");
             } else {
                 Conexion c = new Conexion();
-                String query = "insert into signuptwo values('"+curp+"','"+rfc+"','"+ingreso+"','"+id+"'," +
-                        "'"+id_num+"','"+comprobante+"','"+ocupacion+"','"+tipo_cuenta+"', '"+cliente_existente+"')";
+                String query = "insert into signuptwo values('"+formno+"','"+curp+"','"+rfc+"','"+id+"'," +
+                        "'"+id_num+"','"+comprobante+"','"+ingreso+"','"+ocupacion+"','"+tipo_cuenta+"','"+cliente_existente+"')";
                 c.s.executeUpdate(query);
             }
         } catch (Exception e) {
